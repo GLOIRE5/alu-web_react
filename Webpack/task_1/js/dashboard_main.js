a5f3b728f1ffda40bc0727c63e97cd9e22e200ca
GLOIRE GWIZA <g.gwiza@alustudent.com>
2026-09-06 19:24:21 +0200

    done

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,3 @@
 import $ from 'jquery';
-import { debounce } from 'lodash';
-
-let count = 0;
-
-function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-}
-
-$(document).ready(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="start">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $('#start').on('click', debounce(updateCounter, 300));
-});
+import _ from 'lodash';
+$('#start').on('click', _.debounce(updateCounter, 500));
